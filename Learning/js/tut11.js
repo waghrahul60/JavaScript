d01a9c9c628e1e212afac0b0c92de237e315cede
waghrahul60 <waghrahul60@gmail.com>
2020-11-13 20:36:01 +0530

    tut11

---
+++ Learning/js/tut11.js
@@ -0,0 +1,27 @@
+console.log('This is tut 11 | DOM');
+
+// let a  = window.document;
+// window.alert('Helo raahul ');
+// alert('Without Window Object');
+
+// a = prompt('This is prompt ');
+
+// b = confirm('Confirm Password');
+
+// console.log(a);
+// console.log(b);
+
+
+
+let a = window.document;
+a = window.innerHeight;
+a = window.innerWidth;
+a = outerWidth;
+a = outerHeight;
+
+a = scrollX;
+a = location;
+a = location.toString();
+a = history;
+
+console.log(a);
