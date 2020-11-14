95a1a3feb5d98cb5dfc7b7fc2dc7516285e79de9
waghrahul60 <waghrahul60@gmail.com>
2020-11-14 18:55:53 +0530

    tut21

---
+++ Learning/js/tut21.js
@@ -0,0 +1,6 @@
+console.log('This is tut 21');
+/*
+You have to create a div and  injuct into the page which contains a heading.
+whenever user clicks on the div, it should bc converted into an editable item.
+whwnever user clicks away (blur). save the note into thae local storage
+*/
