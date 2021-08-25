e2e0c5cde02341a965bb9577c19c266e7c28d27f
waghrahul60 <waghrahul60@gmail.com>
2021-08-25 12:10:06 +0530

    JS1

---
+++ NamesteJavaScript/Lec-1/index.js
@@ -0,0 +1,21 @@
+
+
+
+var x =10;
+
+function getName(){
+    console.log("Hello Rahul!!!!");
+}
+
+var getName1 = () => {
+    console.log("Hello Rahul!!!!");
+}
+
+
+var getName2 = function(){
+    console.log("Hello Rahul!!!!");
+}
+
+getName();
+console.log(x);
+console.log(getName);
